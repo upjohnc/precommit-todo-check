e6b661d96ea87759562ff8920e0aea6241944dfc
Chad Upjohn <upjohnc@gmail.com>
2018-11-23 20:27:50 -0500

    tt

--- setup.py
+++ setup.py
@@ -3,6 +3,10 @@ from setuptools import setup
 setup(
     name='pre_commit_todo_check',
     version='0.0.0',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+    ],
     entry_points={
         'console_scripts': [
             'todo_check = pre_commit_hook.todo_check:main',
