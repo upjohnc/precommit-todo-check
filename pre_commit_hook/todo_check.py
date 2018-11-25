0f425fc3b2c029ef9898f61f416b5b129c89f50f
Chad Upjohn <upjohnc@gmail.com>
2018-11-25 14:12:24 -0500

    adding args

--- pre_commit_hook/todo_check.py
+++ pre_commit_hook/todo_check.py
@@ -24,10 +24,11 @@ def check_file(file_name):
 def main(argv=None):
     parser = argparse.ArgumentParser()
     parser.add_argument('filenames', nargs='*', help='Filenames to skip')
+    parser.add_argument('--skip', nargs='?')
     args = parser.parse_args(argv)
 
-    skip_files = args.filenames#('nifi/scripts/score_validation.py', 'stream-generator/stream_generator/qpp/submission_logs.py')
-    print(args.filenames)
+    skip_files = args.skip#kkk('nifi/scripts/score_validation.py', 'stream-generator/stream_generator/qpp/submission_logs.py')
+    print(skip_files)
     files_to_check = [i for i in terminal_run('git ls-files').splitlines() if i not in skip_files]
     print(skip_files, files_to_check)
     return 1
