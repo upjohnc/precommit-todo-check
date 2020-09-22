1415dce2001f0a5449dadacbc4253f13afb158eb
Chad Upjohn <chad.upjohn@stratasan.com>
2020-09-21 21:43:23 -0500

    trying  again

--- setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import find_packages, setup
-
-setup(
-    packages=find_packages(".", exclude=("tests*", "testing*")),
-    entry_points={"console_scripts": ["python-todo-check = todo_check:main"]},
-)
